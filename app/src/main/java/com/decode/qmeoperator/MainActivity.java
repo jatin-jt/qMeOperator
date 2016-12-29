76b1fa031d007dcdcc1029de1fcf538928678b7a
anubhavjindal <anubhav301294@gmail.com>
2016-12-29 16:14:19 +0530

    Firebase added

--- app/src/main/java/com/decode/qmeoperator/MainActivity.java
+++ app/src/main/java/com/decode/qmeoperator/MainActivity.java
@@ -2,10 +2,19 @@ package com.decode.qmeoperator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
         TextView customerAccountId = (TextView) findViewById(R.id.accountIdText);
 
         Button nextCustomer = (Button) findViewById(R.id.callNextButton);
+
+
+        DatabaseReference queueRef = FirebaseDatabase.getInstance().getReference().child("queue");
+        queueRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: here");
+                Log.d(TAG, "onDataChange: "+dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/decode/qmeoperator/models/User.java
@@ -0,0 +1,45 @@
+package com.decode.qmeoperator.models;
+
+/**
+ * Created by anubhavj on 29/12/16.
+ */
+
+public class User {
+
+    String name;
+    String emailId;
+    String contactNo;
+    String accountNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+}
