9a42cce1494315b86fa323f9ba2dd4925d3d069c
anubhavjindal <anubhav301294@gmail.com>
2016-12-29 14:53:38 +0530

    Rough layout ready

--- app/src/main/java/com/decode/qmeoperator/MainActivity.java
+++ app/src/main/java/com/decode/qmeoperator/MainActivity.java
@@ -2,6 +2,7 @@ package com.decode.qmeoperator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView queueLength = (TextView) findViewById(R.id.queueLengthText);
+        TextView customerId = (TextView) findViewById(R.id.currentCustomerText);
+        TextView customerName = (TextView) findViewById(R.id.customerNameText);
+        TextView customerEmail = (TextView) findViewById(R.id.emailText);
+        TextView customerPhone = (TextView) findViewById(R.id.phoneText);
+        TextView customerAccountId = (TextView) findViewById(R.id.accountIdText);
+
+
     }
 }
